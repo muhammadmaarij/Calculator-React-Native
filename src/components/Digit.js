67e51a02aa7a917c5497610c6766f94a4302334a
Muhammad Maarij <muhammadmaarij72gmail.com>
2023-04-14 00:06:22 +0500

    Commit message describing the changes made

--- src/components/Digit.js
+++ src/components/Digit.js
@@ -1,7 +1,7 @@
 import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 
-const Digit = ({text, bgcolor, color, width}) => {
+const Digit = ({text, bgcolor, color, width, onPress}) => {
   var text;
   switch (text) {
     case '*': {
--- src/screens/Calculator.js
+++ src/screens/Calculator.js
@@ -3,7 +3,25 @@ import React, {useState} from 'react';
 import Digit from '../components/Digit';
 
 const Calculator = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(null);
+  const [prevValue, setprevValue] = useState(null);
+  const [operator, setOperator] = useState('');
+
+  function OnHandleSubmit(prevValue, value) {
+    switch (operator) {
+      case '+':
+        prevValue = prevValue + value;
+        break;
+      case '-':
+        prevValue = prevValue - value;
+        break;
+      default:
+        prevValue;
+        // prevValue = prevValue + value;
+        break;
+    }
+  }
+
   return (
     <View style={{flexWrap: 'wrap', backgroundColor: '#393E46', flex: 1}}>
       <View style={{height: 100, marginTop: 70, width: 400}}>
@@ -23,10 +41,14 @@ const Calculator = () => {
           justifyContent: 'space-evenly',
           marginTop: 20,
         }}>
-        <Digit text={'C'} color={'#393E46'} />
-        <Digit text={'AC'} color={'#222831'} />
-        <Digit text={'%'} color={'#222831'} />
-        <Digit text={'/'} color={'#222831'} />
+        <Digit text={'C'} color={'#393E46'} onPress={() => setOperator('C')} />
+        <Digit
+          text={'AC'}
+          color={'#222831'}
+          onPress={() => setOperator('AC')}
+        />
+        <Digit text={'%'} color={'#222831'} onPress={() => setOperator('%')} />
+        <Digit text={'/'} color={'#222831'} onPress={() => setOperator('/')} />
       </View>
       <View
         style={{
@@ -34,10 +56,10 @@ const Calculator = () => {
           justifyContent: 'space-evenly',
           marginTop: 20,
         }}>
-        <Digit text={'7'} />
-        <Digit text={'8'} />
-        <Digit text={'9'} />
-        <Digit text={'*'} color={'#222831'} />
+        <Digit text={'7'} onPress={() => setValue(7)} />
+        <Digit text={'8'} onPress={() => setValue(8)} />
+        <Digit text={'9'} onPress={() => setValue(9)} />
+        <Digit text={'*'} color={'#222831'} onPress={() => setOperator('*')} />
       </View>
       <View
         style={{
@@ -45,10 +67,10 @@ const Calculator = () => {
           justifyContent: 'space-evenly',
           marginTop: 20,
         }}>
-        <Digit text={'4'} color={'white'} />
-        <Digit text={'5'} color={'white'} />
-        <Digit text={'6'} color={'white'} />
-        <Digit text={'-'} color={'#222831'} />
+        <Digit text={'4'} color={'white'} onPress={() => setValue(4)} />
+        <Digit text={'5'} color={'white'} onPress={() => setValue(5)} />
+        <Digit text={'6'} color={'white'} onPress={() => setValue(6)} />
+        <Digit text={'-'} color={'#222831'} onPress={() => setOperator('-')} />
       </View>
       <View
         style={{
@@ -56,10 +78,10 @@ const Calculator = () => {
           justifyContent: 'space-evenly',
           marginTop: 20,
         }}>
-        <Digit text={'1'} color={'white'} />
-        <Digit text={'2'} color={'white'} />
-        <Digit text={'3'} color={'white'} />
-        <Digit text={'+'} color={'#222831'} />
+        <Digit text={'1'} color={'white'} onPress={() => setValue(1)} />
+        <Digit text={'2'} color={'white'} onPress={() => setValue(2)} />
+        <Digit text={'3'} color={'white'} onPress={() => setValue(3)} />
+        <Digit text={'+'} color={'#222831'} onPress={() => setOperator('+')} />
       </View>
       <View
         style={{
@@ -67,9 +89,19 @@ const Calculator = () => {
           justifyContent: 'space-evenly',
           marginTop: 20,
         }}>
-        <Digit text={'0'} color={'white'} width={140} />
-        <Digit text={'.'} color={'white'} />
-        <Digit text={'='} color={'white'} bgcolor={'#222831'} />
+        <Digit
+          text={'0'}
+          color={'white'}
+          width={140}
+          onPress={() => setValue(0)}
+        />
+        <Digit text={'.'} color={'white'} onPress={() => setOperator('.')} />
+        <Digit
+          text={'='}
+          color={'white'}
+          bgcolor={'#222831'}
+          onPress={() => OnHandleSubmit}
+        />
       </View>
     </View>
   );
